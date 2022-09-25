eb24cffdcdb1c8de4963c6e2374363ef42c6e36c
Fox <g_t_campbell@yahoo.com.au>
2022-09-25 11:57:24 +1000

    tutorials saved to file

--- SC3/fileoperations.lua
+++ SC3/fileoperations.lua
@@ -2,21 +2,24 @@ fileops = {}
 
 function fileops.saveTutorial()
     -- ensures tutorial messages are displayed only once and not once per session
-    local savedir = love.filesystem.getSourceBaseDirectory( )
+    local savedir = love.filesystem.getSourceBaseDirectory()
+    -- print("savedir = " .. savedir)
+
     if love.filesystem.isFused() then
         savedir = savedir .. "\\savedata\\"
     else
-        savedir = savedir .. "/Source/savedata/"
+        savedir = savedir .. "/SC3/savedata/"
     end
+    -- print("savedir = " .. savedir)
 
     local savefile
     savefile = savedir .. "tutorial.dat"
+    -- print("savefile = " .. savefile)
+
     local serialisedString = bitser.dumps(tutmsg)
     local success, message = nativefs.write(savefile, serialisedString)
 
     print(success, message)
-
 end
-
 
 return fileops
